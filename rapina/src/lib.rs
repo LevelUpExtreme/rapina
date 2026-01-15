1957910a83053dc6986ef85bdf003fd64d1685ef
Antonio Souza <arfs.antonio@gmail.com>
2026-01-14 22:04:27 -0500

    chore: add module structure
    Signed-off-by: Antonio Souza <arfs.antonio@gmail.com>

--- rapina/src/lib.rs
+++ rapina/src/lib.rs
@@ -1,14 +1,7 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod app;
+pub mod error;
+pub mod extract;
+pub mod handler;
+pub mod response;
+pub mod router;
+pub mod server;
