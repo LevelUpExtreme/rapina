2d94244f51fab26b6555ca8aec32a02c6b440712
LevelUp_Extreme <misakisonoda18@Outlook.com>
2026-02-22 13:10:00 +0900

    Remover trecho desnecessário do arquivo

--- src/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-    printf("Hello, World!\n");
-    printf("Meu nome e Eduardo\n");
-    printf("Meu nome e ricardo\n");
-    printf("Meu nome e Misaki\n");
-    return 0;
-}
