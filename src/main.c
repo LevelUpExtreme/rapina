876ed8833d13bd81ce3627caa1bf91b3e9c77758
LevelUp_Extreme <misakisonoda18@Outlook.com>
2026-02-22 10:16:03 +0900

    add-meu-nome

---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello, World!\n");
+    printf("Meu nome e Eduardo\n");
+    printf("Meu nome e ricardo\n");
+    printf("Meu nome e Misaki\n");
+    return 0;
+}
